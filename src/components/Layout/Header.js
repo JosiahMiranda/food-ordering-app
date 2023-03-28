e63531c9643d65cb35e788b0d1d6346394c24be0
Josiah Miranda <josiahmiranda@hotmail.com>
2023-03-28 19:59:18 +1100

    Added Cart Button with styling to Header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import mealsImage from "../../assets/meals.jpeg";
 import styles from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 
 const Header = props => {
     return <>
         <header className={styles.header}>
             <h1>Meals</h1>
-            <button>Cart</button>
+            <HeaderCartButton />
         </header>
         <div className={styles['main-image']}>
             <img src={mealsImage} alt="A table full of different meals"/>
