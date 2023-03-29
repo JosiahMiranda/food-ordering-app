7fc87f5ddab0595660e6b378c1280a3ddc795637
Josiah Miranda <josiahmiranda@hotmail.com>
2023-03-29 19:34:26 +1100

    added cart items to cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal";
 
 import styles from "./Cart.module.css";
+import CartItem from "./CartItem";
 
 const Cart = props => {
 
-    const cartItems = <ul className={styles['cart-items']}>{[{id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map(item => <li>{item.name}</li>)}</ul>;
+    const cartCtx = useContext(CartContext);
 
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems = cartCtx.items.length > 0;
+
+    const cartItemRemoveHandler = id => {
+
+    };
+
+    const cartItemAddHandler = item => {
+
+    };
+
+    const cartItems = <ul className={styles['cart-items']}>{cartCtx.items.map(item =>
+        <CartItem key={item.id} name={item.name} amount={item.amount}
+            price={item.price} onRemove={cartItemRemoveHandler.bind(null, item.id)} onAdd={cartItemAddHandler.bind(null, item)}>{item.name}</CartItem>)}</ul>;
+        // bind method preconfigures the method to tell it what it should be expecting to be entered. So it
+        // ensures that add handler has an item and remove handler has an id.
     return <Modal onClose={props.onClose}>
         {cartItems}
         <div className={styles.total}>
             <span>Total Amount</span>
-            <span>35.62</span>
+            <span>{totalAmount}</span>
         </div>
         <div className={styles.actions}>
             <button className={styles['button--alt']} onClick={props.onClose}>Close</button>
-            <button className={styles.button}>Order</button>
+            {hasItems && <button className={styles.button}>Order</button>}
         </div>
     </Modal>
 
